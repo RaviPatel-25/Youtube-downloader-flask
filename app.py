37f23fc581a0ef0560551f550d5be72690d1fe8a
Ravi Patel <ravipatel.25.02.2005@gmail.com>
2025-08-15 15:29:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from pytubefix import YouTube
 from pytubefix.cli import on_progress
 import os
 import tempfile
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
